54bddbc2bb51bfcdf858260e94102896c24fa170
unknown <harshivam71011@gmail.com>
2024-01-04 20:52:51 +0530

    Adding files to the project

---
+++ app/src/main/java/com/example/intent/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.intent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var BtnIntent = findViewById<Button>(R.id.button)
+        BtnIntent.setOnClickListener{
+        //opens a new activity
+
+            intent = Intent(applicationContext,Activity_2::class.java)
+            startActivity(intent)
+        }
+    }
+}
